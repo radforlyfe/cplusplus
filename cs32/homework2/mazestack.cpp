d2a0637867afe5cc3a7fa7f5c2c51c8e797027c5
Rad <radhica@ucla.edu>
2019-05-01 00:51:19 -0700

    Q1: is_valid function added

--- cs32/homework2/mazestack.cpp
+++ cs32/homework2/mazestack.cpp
@@ -16,44 +16,56 @@ private:
     int m_c;
 };
 
+constexpr size_t NUM_ROWS = 10;
+constexpr size_t NUM_COLS = 10;
+
+// checks if the maze coordinate is valid to proceed
+bool is_valid(char maze[][10], size_t row, size_t col)
+{
+    if(row < 0 || row >= NUM_ROWS)
+        return false;
+    if(col < 0 || col >= NUM_COLS)
+        return false;
+    if(maze[row][col] != '.')
+        return false;
+    return true;
+}
+
 // Return true if there is a path from (sr,sc) to (er,ec) through the maze;
 // return false otherwise
 bool pathExists(char maze[][10], int sr, int sc, int er, int ec)
 {
     std::stack<Coord> coordStack;
     coordStack.push(Coord(sr,sc));
-    maze[sr][sc] = '*';
+    size_t visit_count =0;
     
     while(!coordStack.empty()){
         Coord current = coordStack.top();
         int r = current.r();
         int c = current.c();
-        
         coordStack.pop();
-        if(r == er && c == ec){                  // current = end coordinate
+        
+        if(maze[r][c] == '.'){                    // mark visited coordinate with count
+            maze[r][c] = visit_count++;
+        }
+        if(r == er && c == ec){                   // current = end coordinate
             return true;
         }
-        if(maze[r+1][c] == '.'){                   // moving SOUTH
-            coordStack.push( Coord(r+1,c) );
-            maze[r+1][c] = '*';
+        if(is_valid(maze, r - 1, c)){                   // moving NORTH
+            coordStack.push(Coord(r - 1, c));
         }
-        if(maze[r][c-1] == '.'){                   // moving WEST
-            coordStack.push( Coord(r,c-1) );
-            maze[r][c-1] = '*';
+        if(is_valid(maze, r + 1, c)){                   // moving SOUTH
+            coordStack.push(Coord(r + 1, c));
         }
-        if(maze[r-1][c] == '.'){                   // moving NORTH
-            coordStack.push( Coord(r-1,c) );
-            maze[r-1][c] = '*';
+        if(is_valid(maze, r, c + 1)){                   // moving EAST
+            coordStack.push( Coord(r, c + 1) );
         }
-        if(maze[r][c+1] == '.'){                   // moving EAST
-            coordStack.push( Coord(r+1,c) );
-            maze[r][c+1] = '*';
+        if(is_valid(maze, r, c - 1)){                   // moving WEST
+            coordStack.push( Coord(r, c - 1) );
         }
     }
     return false;
 }
-
-
 
 int main()
 {
@@ -74,5 +86,4 @@ int main()
         std::cout << "Solvable!" << std::endl;
     else
         std::cout << "Out of luck!" << std::endl;
-        //std::cout<< maze[][];
 }
