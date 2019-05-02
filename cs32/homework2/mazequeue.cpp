b5472387d6f981afcd43b8ef29c29b33994a81f0
Rad <radhica@ucla.edu>
2019-05-01 18:46:13 -0700

    added is_valid function and print grid function

--- cs32/homework2/mazequeue.cpp
+++ cs32/homework2/mazequeue.cpp
@@ -1,6 +1,6 @@
-//
-//  mazequeue.cpp
-//  homework2
+////
+////  mazequeue.cpp
+////  homework2
 
 #include<iostream>
 #include<queue>
@@ -9,6 +9,7 @@ class Coord
 {
 public:
     Coord(int rr, int cc): m_r(rr), m_c(cc) {}
+    Coord() {}
     int r() const { return m_r; }
     int c() const { return m_c; }
 private:
@@ -16,38 +17,66 @@ private:
     int m_c;
 };
 
+constexpr size_t NUM_ROWS = 10;
+constexpr size_t NUM_COLS = 10;
+
+// checks if the maze coordinate is valid to proceed
+bool is_valid(char maze[][10], size_t row, size_t col)
+{
+    if(row < 0 || row >= NUM_ROWS)
+        return false;
+    if(col < 0 || col >= NUM_COLS)
+        return false;
+    if(maze[row][col] != '.')
+        return false;
+    return true;
+}
+
+// print grid
+void print_grid(char maze[][10])
+{
+    for(size_t r = 0; r < NUM_ROWS; ++r){
+        for(size_t c = 0; c < NUM_COLS; ++c){
+            std::cout << maze[r][c] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 // Return true if there is a path from (sr,sc) to (er,ec) through the maze;
 // return false otherwise
 bool pathExists(char maze[][10], int sr, int sc, int er, int ec)
 {
     std::queue<Coord> coordQueue;
-    coordQueue.push( Coord(sr,sc) );
-    maze[sr][sc] = '*';
+    std::queue<Coord> results;
+    coordQueue.push(Coord(sr,sc));
+    size_t visit_count = 0;
     
     while(!coordQueue.empty()){
-        Coord current = coordQueue.back();        // change for Queue--front? or back?
+        Coord current = coordQueue.front();
         int r = current.r();
         int c = current.c();
         coordQueue.pop();
+        results.push(current);
         
-        if(r == er && c == ec){                  // current = end coordinate
+        if(maze[r][c] == '.'){                    // mark visited coordinate with count
+            visit_count++;
+            maze[r][c] = static_cast<char>(visit_count);
+        }
+        if(r == er && c == ec){                   // current = end coordinate
             return true;
         }
-        if(maze[r+1][c] == '.'){                   // moving SOUTH
-            coordQueue.push( Coord(r+1,c) );
-            maze[r+1][c] = '*';
+        if(is_valid(maze, r - 1, c)){                   // moving NORTH
+            coordQueue.push(Coord(r - 1, c));
         }
-        if(maze[r][c-1] == '.'){                   // moving WEST
-            coordQueue.push( Coord(r,c-1) );
-            maze[r][c-1] = '*';
+        if(is_valid(maze, r + 1, c)){                   // moving SOUTH
+            coordQueue.push(Coord(r + 1, c));
         }
-        if(maze[r-1][c] == '.'){                   // moving NORTH
-            coordQueue.push( Coord(r-1,c) );
-            maze[r-1][c] = '*';
+        if(is_valid(maze, r, c + 1)){                   // moving EAST
+            coordQueue.push( Coord(r, c + 1) );
         }
-        if(maze[r][c+1] == '.'){                   // moving EAST
-            coordQueue.push( Coord(r+1,c) );
-            maze[r][c+1] = '*';
+        if(is_valid(maze, r, c - 1)){                   // moving WEST
+            coordQueue.push( Coord(r, c - 1) );
         }
     }
     return false;
@@ -67,10 +96,12 @@ int main()
         { 'X','.','.','.','.','.','.','.','.','X' },
         { 'X','X','X','X','X','X','X','X','X','X' }
     };
+    print_grid(maze);
     
-    if (pathExists(maze, 3,5, 8,8))
+    if (pathExists(maze, 3,5, 8,8)){
         std::cout << "Solvable!" << std::endl;
+        print_grid(maze);
+    }
     else
         std::cout << "Out of luck!" << std::endl;
-    //std::cout<< maze[][]; // print maze
 }
--- cs32/homework2/mazestack.cpp
+++ cs32/homework2/mazestack.cpp
@@ -9,6 +9,7 @@ class Coord
 {
 public:
     Coord(int rr, int cc): m_r(rr), m_c(cc) {}
+    Coord() {}
     int r() const { return m_r; }
     int c() const { return m_c; }
 private:
@@ -31,22 +32,37 @@ bool is_valid(char maze[][10], size_t row, size_t col)
     return true;
 }
 
+// print grid
+void print_grid(char maze[][10])
+{
+    for(size_t r = 0; r < NUM_ROWS; ++r){
+        for(size_t c = 0; c < NUM_COLS; ++c){
+            std::cout << maze[r][c] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 // Return true if there is a path from (sr,sc) to (er,ec) through the maze;
 // return false otherwise
 bool pathExists(char maze[][10], int sr, int sc, int er, int ec)
 {
     std::stack<Coord> coordStack;
+    // Coord results[12];
+    std::stack<Coord> results;
     coordStack.push(Coord(sr,sc));
-    size_t visit_count =0;
+    size_t visit_count = 0;
     
     while(!coordStack.empty()){
         Coord current = coordStack.top();
         int r = current.r();
         int c = current.c();
         coordStack.pop();
+        results.push(current);
         
         if(maze[r][c] == '.'){                    // mark visited coordinate with count
-            maze[r][c] = visit_count++;
+            visit_count++;
+            maze[r][c] = static_cast<char>(visit_count);
         }
         if(r == er && c == ec){                   // current = end coordinate
             return true;
@@ -81,9 +97,12 @@ int main()
         { 'X','.','.','.','.','.','.','.','.','X' },
         { 'X','X','X','X','X','X','X','X','X','X' }
     };
+     print_grid(maze);
     
-    if (pathExists(maze, 3,5, 8,8))
+    if (pathExists(maze, 3,5, 8,8)){
         std::cout << "Solvable!" << std::endl;
+        print_grid(maze);
+    }
     else
         std::cout << "Out of luck!" << std::endl;
 }
