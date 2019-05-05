c23a7e915fe13a5295610632d0d13a40666f5b5a
Rad <radhica@ucla.edu>
2019-05-05 16:19:02 -0700

    updated complex(string) constructor, added comments

--- p10b/hwk5/Complex.cpp
+++ p10b/hwk5/Complex.cpp
@@ -2,100 +2,104 @@
 //  Assignment5
 
 #include "Complex.h"
+namespace { // to declare the "real" and "imag" as constexpr
+    constexpr char REAL[] = "real";
+    constexpr char IMAG[] = "imag";
+}
 
-Complex::Complex(std::string str){
-    if(str == ""){
-        throw std::logic_error("empty string");
+// Constructor that accepts a string input - parses the string into a Complex number
+Complex::Complex(const std::string& string_){
+    auto str = string_;
+    if(str.empty()){                                             // throw logic error exception for empty string
+        throw std::logic_error("bad input string: " + string_);
     }
-    if(str[0] == 'i'){
+    if(str.front() == 'i'){                                      // string starting with i : replace with 1i
         str.replace(0, 1, "1i");
     }
     size_t minus_i = str.find("-i");
-    while(minus_i != std::string::npos){
+    while(minus_i != std::string::npos){                         // find -i in string : replace with -1i
         str.replace(minus_i, 2, "-1i");
         minus_i = str.find("-i");
     }
     size_t plus_i = str.find("+i");
-    while(plus_i != std::string::npos){
+    while(plus_i != std::string::npos){                          // find +i in string : replace with +1i
         str.replace(plus_i, 2, "+1i");
         plus_i = str.find("+i");
     }
-    std::istringstream ss(str);
-    double _real = 0, _imag = 0, _temp =0;
-    while(!ss.eof()){
-        ss >> _temp;
-        if(ss.peek() == '+' || ss.peek() == '-'){
-            _real += _temp;
-            ss.get();
+    size_t char_i = str.find("i");
+    while(char_i != std::string::npos){                          // find i in string : replace with $ symbol
+        str.replace(char_i, 1, "$");
+        char_i = str.find("i");
+    }
+    std::istringstream ss(str);                                  // make input string stream from str
+    double _real = 0.0, _imag = 0.0, _temp = 0.0;
+    while(ss.peek() != -1){                                      // loop until peek != -1 (end)
+        ss >> _temp;                                             // read from the stream into _temp
+        if(ss.fail()){                                           // throw exception if string cannot be parsed
+            throw std::logic_error("bad input string: " + string_);
         }
-        else if(ss.peek() == 'i'){
+        if(ss.peek() == '+' || ss.peek() == '-' || ss.peek() == EOF){  // if next char is +/-/end, add to real
+            _real += _temp;
+        }
+        else if(ss.peek() == '$'){                                     // if next char is $(i), add to imag
             _imag += _temp;
-            ss.get();
+            ss.get();                                                  // moves to the next character
         }
     }
-    real_ = _real;
-    imag_ = _imag;
+    real_ = _real;                                                    // set real component
+    imag_ = _imag;                                                    // set imag component
 }
 
+// Conversion operator from Complex to String
 Complex::operator std::string() const{
-    std::ostringstream oss;
-    if(imag_ > 0)
-        oss << real_ << '+' << imag_ << 'i';
-    else
-        oss << real_ << imag_ << 'i';
-    std::string s = oss.str();
-    return s;
+    std::ostringstream oss;                                        // makes an output string stream
+    oss << *this;                                                  // calls the << operator
+    return oss.str();                                              // returns the string storing content of stream
 }
-        
+
+// Subscript operator overloaded on const:
 const double& Complex::operator[](std::string str) const{
-    if(str == "real")
+    if(str == REAL)                                                // if str is "real" return real component
         return real_;
-    if(str == "imag")
+    if(str == IMAG)                                                // if str is "imag" return imaginary component
         return imag_;
     else
-        throw std::out_of_range("invalid index: " + str);
+        throw std::out_of_range("invalid index: " + str);          // throw out_of_range exception otherwise
 }
 
 double& Complex::operator[](std::string str){
-    if(str == "real")
+    if(str == REAL)                                                // if str is "real" assign real component
         return real_;
-    if(str == "imag")
+    if(str == IMAG)                                                // if str is "real" assign real component
         return imag_;
     else
-        throw std::out_of_range("invalid index: " + str);
+        throw std::out_of_range("invalid index: " + str);          // throw out_of_range exception otherwise
 }
-    
+
+// operator<< to print the complex number with an output stream
 std::ostream& operator<<(std::ostream& out, const Complex& c){
-    if(c["real"] == 0 && c["imag"] == 0){
-        out << " ";
+    if(c[REAL] != 0 || c[IMAG] == 0){                             // always prints real unless real=0 and imag!=0
+        out << c[REAL];
     }
-    else if(c["real"] == 0 && c["imag"] > 0){
-        out << c["imag"] << "i";
+    if(c[REAL] != 0 && c[IMAG] > 0){                              // print + operator for real!=0 and imag>0
+        out << '+';
     }
-    else if(c["real"] == 0 && c["imag"] < 0){
-        out << c["imag"] << "i";
+    if(c[IMAG] == 1){                                             // print i instead of 1i
+        out << "i";
     }
-    else if (c["imag"] == 0){
-        out << c["real"];
+    else if (c[IMAG] == -1){                                      // print -i instead of -1i
+        out << "-i";
     }
-    else if (c["imag"] == 1){
-        out << c["real"] << "+i";
-    }
-    else if (c["imag"] == -1){
-        out << c["real"] << "-i";
-    }
-    else if(c["imag"] > 0){
-        out << c["real"] << "+" << c["imag"] << "i";
-    }
-    else if (c["imag"] < 0){
-        out << c["real"] << c["imag"] << "i";
+    else if(c[IMAG] != 0){                                        // print imag i for all other cases when imag!=0
+        out << c[IMAG] << "i";
     }
     return out;
 }
 
+// operator>> to read in two double values, separated by white space
 std::istream& operator>>(std::istream& in, Complex& c){
-    in >> c["real"] >> c["imag"];
+    in >> c[REAL] >> c[IMAG];                                     // set real and imaginary components using istream
     if(in.fail())
-        throw std::runtime_error("bad inputs for reading");
+        throw std::runtime_error("bad inputs for reading");       // throw exception if fail to read
     return in;
 }
--- p10b/hwk5/Complex.h
+++ p10b/hwk5/Complex.h
@@ -12,29 +12,123 @@
 class Complex
 {
 public:
+    /**
+     constexpr constructor which behaves like a default constructor, through the use of default arguments:
+        constructs a complex number from real and imaginary parts
+     @param real : the real component of complex number
+     @param imaginary : the imaginary component of complex number
+     */
     constexpr Complex(double real=0, double imaginary=0);
-    Complex(std::string str);
     
+    /**
+     constructor that accepts a string input that can parse a string into a complex number
+     @param str : string that can be parsed into complex number
+     */
+    Complex(const std::string& str);
+    
+    /**
+     binary operator+= to add two complex numbers, changing the lhs
+     @param other : rhs which is added to the *this (lhs)
+     @return updated Complex number
+     */
     constexpr Complex& operator+=(const Complex& other);
+    
+    /**
+     binary operator-= to subtract two complex numbers, changing the lhs
+     @param other : rhs which is subtracted to the *this (lhs)
+     @return updated Complex number
+     */
     constexpr Complex& operator-=(const Complex& other);
+    
+    /**
+     binary operator*= to multiply two complex numbers, changing the lhs
+     @param other : rhs which is multiplied to the *this (lhs)
+     @return updated Complex number
+     */
     constexpr Complex& operator*=(const Complex& other);
+    
+    /**
+     binary operator/= to divide two complex numbers, changing the lhs
+     @param other : rhs which divides the *this (lhs)
+     @return updated Complex number
+     */
     constexpr Complex& operator/=(const Complex& other);
     
+    /**
+     unary operator+ to return a copy of the complex number.
+     @return copy of the Complex number
+     */
     constexpr Complex operator+() const;
+    
+    /**
+     unary operator- to return a complex number with negated real and imaginary parts
+     @return copy of the negated Complex number (negated real and negated imaginary)
+     */
     constexpr Complex operator-() const;
+    
+    /**
+     unary operator~ to return the conjugate of the complex number
+     @return copy of the conjugated Complex number (negated imaginary)
+     */
     constexpr Complex operator~() const;
     
-    constexpr Complex& operator++();  //prefix
-    constexpr Complex operator++(int); //postfix
-    constexpr Complex& operator--(); //prefix
-    constexpr Complex operator--(int); //postfix
+    /**
+     prefix (unary) operator++: real part is incremented by 1
+     @return real part incremented by 1
+     */
+    constexpr Complex& operator++();
     
-    const double& operator[](std::string str) const; 
+    /**
+     postfix (binary) operator++: real part is incremented by 1
+     @param unused : unused parameter
+     @return copy of the Complex number (no change)
+     */
+    constexpr Complex operator++(int unused); //postfix
+    
+    /**
+     prefix (unary) operator--: real part is decremented by 1
+     @return real part decremented by 1
+     */
+    constexpr Complex& operator--();
+    
+    /**
+     postfix (binary) operator--: real part is decremented by 1
+     @param unused : unused parameter
+     @return copy of the Complex number (no change)
+     */
+    constexpr Complex operator--(int unused);
+    
+    /**
+     subscript operator overloaded on const: takes either “real” or “imag” as arguments and returns the real or imaginary parts of the Complex number respectively
+     @param str : string if "real" or "imag" gives the value of real or imaginary component
+     @return const double& : reads the real or imaginary parts of the Complex number
+     */
+    const double& operator[](std::string str) const;
+    
+    /**
+     subscript operator: takes either “real” or “imag” as arguments and returns the real or imaginary parts of the Complex number respectively
+     @param str : string if "real" or "imag" assigns the value to real or imaginary component
+     @return double& : assigns the real or imaginary parts of the Complex number
+     */
     double& operator[](std::string str);
     
-    constexpr void operator()(); // call operator
-    operator std::string() const; // conversion operator
+    /**
+     call operator taking no arguments setting the complex number to 0
+     */
+    constexpr void operator()();
+    
+    /**
+     conversion operator from Complex to std::string
+     @return string converted from Complex
+    */
+    operator std::string() const;
 
+    /**
+     comparison operators operator< : compared lexicographically - since non-member function declared as friend
+     @param lhs Complex number compared
+     @param rhs Complex number compared
+     @return true if lhs is less than rhs
+     */
     friend constexpr bool operator<(const Complex& lhs, const Complex& rhs);
     
 private:
@@ -44,23 +138,105 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Non-member functions declarations:
+
+/**
+ binary operator+ to add two complex numbers, returning sum
+ @param lhs : Complex number passed as copy of lhs
+ @param rhs : Complex number passed as reference to const
+ @return updated lhs Complex number which is the sum of lhs and rhs
+ */
 constexpr Complex operator+(Complex lhs, const Complex& rhs);
+
+/**
+ binary operator- to subtract two complex numbers, returning difference
+ @param lhs : Complex number passed as copy of lhs
+ @param rhs : Complex number passed as reference to const
+ @return updated lhs Complex number which is the difference of lhs and rhs
+ */
 constexpr Complex operator-(Complex lhs, const Complex& rhs);
+
+/**
+ binary operator* to multiply two complex numbers, returning product
+ @param lhs : Complex number passed as copy of lhs
+ @param rhs : Complex number passed as reference to const
+ @return updated lhs Complex number which is the product of lhs and rhs
+ */
 constexpr Complex operator*(Complex lhs, const Complex& rhs);
+
+/**
+ binary operator/ to divide two complex numbers, returning quotient
+ @param lhs : Complex number passed as copy of lhs
+ @param rhs : Complex number passed as reference to const
+ @return updated lhs Complex number which is the quotient of lhs and rhs
+ */
 constexpr Complex operator/(Complex lhs, const Complex& rhs);
 
+/**
+ operator<< to print/ display the Complex number with an output stream
+ @param out : output stream
+ @param c : Complex number passed as reference to const
+ @return print out of the Complex number with an output stream
+ */
 std::ostream& operator<<(std::ostream& out, const Complex& c);
-std::istream& operator>>(std::istream& in, Complex& c);
-constexpr Complex operator""_i(long double c); // user-defined literal expression
 
+/**
+ operator>> to read in two double values, separated by white space
+ @param in : input stream
+ @param c : Complex number passed as reference to const
+ @return print out of the Complex number with an output stream
+ */
+std::istream& operator>>(std::istream& in, Complex& c);
+
+/** user-defined literal expression: so that code expression will be converted into complex number
+ @param c : long double
+ @return Complex number
+ */
+constexpr Complex operator""_i(long double c);
+
+/**
+ comparison operators operator>= : compared lexicographically
+ @param lhs Complex number compared
+ @param rhs Complex number compared
+ @return true if lhs is greater than or equal to rhs
+ */
 constexpr bool operator>=(const Complex& lhs, const Complex& rhs);
+
+/**
+ comparison operators operator== : compared lexicographically
+ @param lhs Complex number compared
+ @param rhs Complex number compared
+ @return true if lhs is equal to rhs
+ */
 constexpr bool operator==(const Complex& lhs, const Complex& rhs);
+
+/**
+ comparison operators operator!= : compared lexicographically
+ @param lhs Complex number compared
+ @param rhs Complex number compared
+ @return true if lhs is not equal to rhs
+ */
 constexpr bool operator!=(const Complex& lhs, const Complex& rhs);
+
+/**
+ comparison operators operator> : compared lexicographically
+ @param lhs Complex number compared
+ @param rhs Complex number compared
+ @return true if lhs is greater than rhs
+ */
 constexpr bool operator>(const Complex& lhs, const Complex& rhs);
+
+/**
+ comparison operators operator<= : compared lexicographically
+ @param lhs Complex number compared
+ @param rhs Complex number compared
+ @return true if lhs is less than or equal to rhs
+ */
 constexpr bool operator<=(const Complex& lhs, const Complex& rhs);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // INLINE Complex class member function definitions:
+
+// must initialize in the initializer list
 constexpr Complex::Complex(double real, double imaginary): real_(real), imag_(imaginary){}
 
 constexpr Complex& Complex::operator+=(const Complex& other){
@@ -94,41 +270,42 @@ constexpr Complex& Complex::operator/=(const Complex& other){
     return *this;
 }
 
-constexpr Complex Complex::operator+() const{
+constexpr Complex Complex::operator+() const{             // unary operator+
     return Complex(real_, imag_);
 }
-constexpr Complex Complex::operator-() const{
+constexpr Complex Complex::operator-() const{             // unary operator-
     return Complex(-real_, -imag_);
 }
-constexpr Complex Complex::operator~() const{
+constexpr Complex Complex::operator~() const{            // unary operator~
     return Complex(real_, -imag_);
 }
-constexpr Complex& Complex::operator++(){ //prefix
+constexpr Complex& Complex::operator++(){                // prefix ++ unary operator
     ++real_;
     return *this;
 }
-constexpr Complex Complex::operator++(int){ //postfix
+constexpr Complex Complex::operator++(int unused){       // postfix ++ binary operator
     Complex temp(*this);
     ++(*this);
     return temp;
 }
-constexpr Complex& Complex::operator--(){
+constexpr Complex& Complex::operator--(){                // prefix -- unary operator
     --real_;
     return *this;
 }
-constexpr Complex Complex::operator--(int){
+constexpr Complex Complex::operator--(int unused){       // prefix -- binary operator
     Complex temp(*this);
     --(*this);
     return temp;
 }
 
-constexpr void Complex::operator()(){ // call operator
-    real_ =0;
+constexpr void Complex::operator()(){                    // call operator
+    real_ = 0;
     imag_ = 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Non-member functions definitions:
+
 constexpr Complex operator+(Complex lhs, const Complex& rhs){
     return lhs += rhs;
 }
@@ -142,37 +319,37 @@ constexpr Complex operator/(Complex lhs, const Complex& rhs){
     return lhs /= rhs;
 }
 constexpr Complex operator""_i(long double c){
-    return Complex(0, c);
+    return Complex(0, c);                                           // calling Complex constructor with real=0
 }
-constexpr bool operator<(const Complex& lhs, const Complex& rhs){
-    if (lhs.real_ < rhs.real_)
+constexpr bool operator<(const Complex& lhs, const Complex& rhs){   // non-member function operator< as friend
+    if (lhs.real_ < rhs.real_)                                      // comparison for real components
         return true;
-    if(lhs.real_ == rhs.real_ && lhs.imag_ < rhs.imag_)
+    if(lhs.real_ == rhs.real_ && lhs.imag_ < rhs.imag_)             // comparison for imag if reals are equal
         return true;
     return false;
 }
 constexpr bool operator>=(const Complex& lhs, const Complex& rhs){
-    if(!(lhs < rhs))
+    if(!(lhs < rhs))                                               // !(a<b) == (a>=b)
         return true;
     return false;
 }
 constexpr bool operator==(const Complex& lhs, const Complex& rhs){
-    if(!(lhs < rhs) && !(rhs < lhs))
+    if(!(lhs < rhs) && !(rhs < lhs))                              // !(a<b) && !(b<a) == (a==b)
         return true;
     return false;
 }
 constexpr bool operator!=(const Complex& lhs, const Complex& rhs){
-    if(!(lhs == rhs))
+    if(!(lhs == rhs))                                             // !(a=b) == (a!=b)
         return true;
     return false;
 }
 constexpr bool operator>(const Complex& lhs, const Complex& rhs){
-    if(!(lhs < rhs) && lhs != rhs)
+    if(!(lhs < rhs) && lhs != rhs)                                // !(a<b) && (a!=b) == (a>b)
         return true;
     return false;
 }
 constexpr bool operator<=(const Complex& lhs, const Complex& rhs){
-    if(!(lhs > rhs))
+    if(!(lhs > rhs))                                              // !(a>b) == (a<=b)
         return true;
     return false;
 }
