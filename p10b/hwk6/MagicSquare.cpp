f8e83d0d6bd76fa73fd5cfb54c8e802a63ed7447
Rad <radhica@ucla.edu>
2019-05-18 23:25:06 -0700

    without permuation implementation

--- p10b/hwk6/MagicSquare.cpp
+++ p10b/hwk6/MagicSquare.cpp
@@ -1,41 +1,39 @@
-//
-//  MagicSquare.cpp
+//  MagicSquare.cpp - NO PERMUTATIONS
 //  assignment6
 
 #include "MagicSquare.h"
 
-//constructor
-MagicSquare::MagicSquare(const std::vector<std::vector<int>>& vec): data_(vec){}
+MagicSquare::MagicSquare(const std::vector<std::vector<int>>& vec): data_(vec){
+    size_t n = data_.size();
+    for(size_t index = 0; index < n*n; ++index){
+        if(getnum(index) != 0){
+            taken_.insert(getnum(index));
+        }
+        else{
+            unused_indices.push_back(index);
+        }
+    }
+}
 
 bool MagicSquare::empty(size_t row, size_t col) const{
-    if(data_[row][col] == 0){ // if value is 0, then return true
-        return true;
-    }
-    return false;
+    return data_[row][col] == 0;
 }
 
 bool MagicSquare::taken(int i){
-    for(size_t row = 0; row < data_.size(); ++row){ // loop through all rows
-        for(size_t col = 0; col < data_.size(); ++col){ //loop through all columns
-            if(i == data_[row][col]){
-                return true;
-            }
-        }
-    }
-    return false;
+    return taken_.find(i) != taken_.end();
 }
 
 bool MagicSquare::checkRow() const{
     size_t n = data_.size();
     size_t target = n * ((n * n) + 1)/2;
-    for(size_t i = 0; i < n; ++i){ //loop through one dimensional row
+    for(size_t i = 0; i < n; ++i){
         int sum_row = 0;
         int sum_col = 0;
-        for(size_t j = 0; j < n; ++j){ // loop through columns
+        for(size_t j = 0; j < n; ++j){
             sum_row += data_[i][j];
             sum_col += data_[j][i];
         }
-        if(sum_row != target || sum_col >= target){
+        if(sum_row != target || sum_col > target){
             return false;
         }
     }
@@ -47,16 +45,23 @@ bool MagicSquare::checkValid() const{
     size_t target = n * (n * n + 1)/2;
     int sum_diag1 = 0;
     int sum_diag2 = 0;
-    if(checkRow()){ // if checkRow is valid proceed
-        for(size_t i = 0; i < n; ++i){  // loop through the dimension of square
-            sum_diag1 += data_[i][i];
-            sum_diag2 += data_[i][n - 1 - i];
-        }
+    if(!checkRow()){
+        return false;
     }
-    if(sum_diag1 != target || sum_diag2 != target){ // if diag is not equal to target return false
+    for(size_t i = 0; i < n; ++i){
+        sum_diag1 += data_[i][i];
+        sum_diag2 += data_[i][n - 1 - i];
+    }
+    if(sum_diag1 != target || sum_diag2 != target){
         return false;
     }
     return true;
+}
+
+int& MagicSquare::getnum(size_t index){
+    size_t i = index / data_.size();
+    size_t j = index % data_.size();
+    return data_[i][j];
 }
 
 int MagicSquare::getnum(size_t index) const{
@@ -65,7 +70,11 @@ int MagicSquare::getnum(size_t index) const{
     return data_[i][j];
 }
 
-
+void MagicSquare::setnum(size_t index, int value){
+    size_t i = index / data_.size();
+    size_t j = index % data_.size();
+    data_[i][j] = value;
+}
 
 int MagicSquare::getcount() const{
     return count_solutions;
@@ -73,25 +82,30 @@ int MagicSquare::getcount() const{
 
 void MagicSquare::solveSquare(size_t index){
     size_t n = data_.size();
-    if(index == n * n){   //base case of recursion step
-        if(checkValid()){   // checks if whole square is valid
+    size_t row = index / data_.size();
+    size_t col = index % data_.size();
+    if(col == 0 && row != 0){                                  //checks if each row is valid
+        checkRow();
+    }
+    if(index == n * n){                                        //base case of recursion step
+        if(checkValid()){                                      // checks if whole square is valid
             ++count_solutions;
             std::cout << data_ << '\n';
         }
     }
-    else{  //if index not at last position
-        size_t i = index / data_.size();
-        size_t j = index % data_.size();
-        if(empty(i,j)){  // if value at index is empty
-            for(int value = 1; value <= n * n; ++value){ //loop through all index
-                if(!taken(value)){ // ensure value not in taken
-                    data_[i][j] = value;
+    else{                                                      //if index not at last position
+        if(empty(row, col)){                                   // if value at index is 0
+            for(int value = 1; value <= n * n; ++value){
+                if(!taken(value)){                             // ensure value not in taken
+                    data_[row][col] = value;
+                    taken_.insert(value);
                     solveSquare(index+1);
-                    data_[i][j] = 0;
+                    data_[row][col] = 0;
+                    taken_.erase(value);
                 }
             }
         }
-        else{ // if not empty
+        else{                                                  // if value at index is not 0
             solveSquare(index+1);
         }
     }
