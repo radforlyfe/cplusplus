3bea102a640eaeee5896b51a14622b0e5f05130d
Rad <radhica@ucla.edu>
2019-05-24 22:04:19 -0700

    insert and find

--- p10b/hwk8/BinarySearchTree.cpp
+++ p10b/hwk8/BinarySearchTree.cpp
@@ -8,29 +8,69 @@ namespace pic10b{
 
     Tree::Tree() noexcept: root(nullptr), size_(0){}
     
+    Tree::iterator Tree::find(const double value) const{
+        Tree::node *curr = root;
+        if(curr == nullptr){
+            return end();
+        }
+        for(;;){
+            if(value == curr->value_){
+                return iterator(root, this);
+            }
+            else if(value < curr->value_){
+                if(curr->left != nullptr){
+                    curr = curr->left;
+                }
+                else{
+                    return end();
+                }
+            }
+            else if(value > curr->value_){
+                if(curr->right != nullptr){
+                    curr = curr->right;
+                }
+                else{
+                    return end();
+                }
+            }
+        }
+    }
     
-
-
+    void Tree::insert(double value){
+        if(root == nullptr){
+            root = new node(value);
+            return;
+        }
+        Tree::node *curr = root;
+        for(;;){
+            if(value == curr->value_){
+                return;
+            }
+            if(value < curr->value_){
+                if(curr->left != nullptr){
+                    curr = curr->left;
+                }
+                else{
+                    curr->left = new node(value);
+                    return;
+                }
+            }
+            if(value > curr->value_){
+                if(curr->right != nullptr){
+                    curr = curr->right;
+                }
+                else{
+                    curr->right = new node(value);
+                    return;
+                }
+            }
+        }
+    }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     Tree::node::node(double value): value_(value), left(nullptr), right(nullptr) {} //node stores value with children left and right both null
 
-    void Tree::node::insertNode(node*){}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    Tree::const_iterator::const_iterator(node* n, const Tree* t): curr(n), container(t) {}
-    
-    Tree::const_iterator& Tree::const_iterator::operator++(){      //prefix ++ unary operator
-        if(curr == nullptr){
-            throw std::logic_error("Invalid address");
-        }
-        else{
-            
-        }
-        return *this;
-    }
-
-    
-}
+    Tree::iterator::iterator(Tree::node* n, const Tree* t): curr(n), container(t) {}
